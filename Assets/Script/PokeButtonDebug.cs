896e17126313a0e82e6924c690b2c86b683debe7
EntNdo19 <94893384+EntNdo19@users.noreply.github.com>
2026-02-17 15:21:17 +0100

    first commit

---
+++ Assets/Script/PokeButtonDebug.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PokeButtonDebug : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnButtonPressed()
+    {
+        Debug.Log("Button gedrückt");
+    }
+}
